9c34ad35cd97df3566768dee1f8da4df7d4737c0
wonjunghyun <gankwal@fssolution.co.kr>
2022-06-02 10:59:43 +0900

    ~

--- network/backbone/resnet/resnet.py
+++ network/backbone/resnet/resnet.py
@@ -1,43 +1,44 @@
-from functools import partial
-from typing import Type, Any, Callable, Union, List, Optional
-
 import torch
 import torch.nn as nn
 
-from torch import Tensor
+from torch.utils import load_state_dict_from_url
 
-def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
+def conv3x3(in_planes, out_planes, stride = 1, groups = 1, dilation = 1):
     return nn.Conv2d(
         in_planes,
         out_planes,
-        kernel_size=3,
-        stride=stride,
-        padding=dilation,
-        groups=groups,
-        bias=False,
-        dilation=dilation,
+        kernel_size = 3,
+        stride = stride,
+        padding = dilation,
+        groups = groups,
+        bias = False,
+        dilation = dilation,
     )
 
-
-def conv1x1(in_planes: int, out_planes: int, stride: int =1) -> nn.Conv2d:
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-
+def conv1x1(in_planes, out_planes, stride = 1):
+    return nn.Conv2d(
+        in_planes, 
+        out_planes, 
+        kernel_size = 1, 
+        stride = stride, 
+        bias = False
+    )
 
 class BasicBlock(nn.Module):
     expansion: int = 1
     
     def __init__(
         self,
-        inplanes: int,
-        planes: int,
-        stride: int = 1,
-        downsample: Optional[nn.Module] = None,
-        groups: int = 1,
-        base_width: int = 64,
-        dilation: int = 1,
-        norm_layer: Optional[Callable(..., nn.Module)] = None,
-    ) -> None:
-        super().__init__()
+        inplanes,
+        planes,
+        stride = 1,
+        downsample = None,
+        groups = 1,
+        base_width = 64,
+        dilation = 1,
+        norm_layer = None,
+    ):
+        super(BasicBlock, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
         if groups != 1 or base_width != 64:
@@ -53,8 +54,9 @@ class BasicBlock(nn.Module):
         self.downsample = downsample
         self.stride = stride
         
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, x):
         identity = x
+        
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
@@ -70,5 +72,66 @@ class BasicBlock(nn.Module):
         
         return out
         
-class BottleNeckBlock(nn.Module):
+class BottleNeck(nn.Module):
+    expansion: int = 4
     
+    def __init__(
+        self,
+        inplanes,
+        planes,
+        stride = 1,
+        downsample = None,
+        groups = 1,
+        base_width = 1,
+        dilation = 1,
+        norm_layer = None,
+    ):
+        super(BottleNeck, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace = True)
+        self.stride = stride
+        
+    def forward(self, x):
+        identity = x
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        
+        out = self.conv3(out)
+        out = self.bn3{out}
+        
+        if self.downsample is not None:
+            identity = self.downsample(x)
+            
+        out += identity
+        out = self.relu(out)
+        
+        return out
+    
+class ResNet(nn.Module):
+    def __init__(
+        self,
+        block,
+        layers,
+        num_classed = 1000,
+        zero_init_resifual = False,
+        groups = 1,
+        width_per_group = 64,
+        replace_stride_with_dilation = None,
+        norm_layer = None,
+    ):
+        super(ResNet, self).__init__()
+        
